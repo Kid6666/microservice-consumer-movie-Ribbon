0c3fdabae41b380f379b18f72da95e3a846217c7
kid6666 <1012468713@qq.com>
2019-10-27 12:53:18 +0800

    修改ribbon方式，改为Feign的方式

--- src/main/java/com/lejx/microserviceconsumermovie/user/MovieController.java
+++ src/main/java/com/lejx/microserviceconsumermovie/user/MovieController.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
 public class MovieController {
@@ -16,20 +17,11 @@ public class MovieController {
     private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
 
     @Autowired
-    private RestTemplate restTemplate;
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
+    private UserFeignClient userFeignClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable long id){
-        return restTemplate.getForObject("http://MICROSERVICE-PROVIDER-USER/"+id,User.class);
-    }
-
-    @GetMapping("/log-user-instance")
-    public  void logUserInstance(){
-        ServiceInstance serviceInstance = loadBalancerClient.choose("MICROSERVICE-PROVIDER-USER");
-        logger.info("{}:{}:{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
-//        System.out.println(serviceInstance.getServiceId()+" : "+serviceInstance.getHost()+" : "+serviceInstance.getPort());
+        return userFeignClient.findByID(id);
     }
 
 }
